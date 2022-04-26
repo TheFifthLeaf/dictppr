7553f6d6279c5916d3187e529bbd8fc859db6108
Dawid Jach <jach.developer@protonmail.com>
2022-04-26 19:34:24 +0200

    Add new-item getter

--- dictppr.py
+++ dictppr.py
@@ -18,3 +18,32 @@ def check_nesting(items: list) -> int:
         if isinstance(item, list):
             count += check_nesting(item)
     return count
+
+
+def get_new(item: any) -> any:
+    """
+    Checks whether the given item
+    is as flattened as possible.
+
+    Parameters
+    ----------
+    item
+        Item to be checked.
+
+    Returns
+    ----------
+    New item.
+    """
+    if isinstance(item, dict):
+        keys = list(item.keys())
+        item = flatten(item)
+        item = get_new(item)
+        if isinstance(item, list) and len(keys) > 1:
+            if not check_nesting(item):
+                item = map(lambda k, i: f"{k}:{i}", keys, item)
+                item = map(lambda i: i.replace("'", ""), item)
+                item = list(item)
+    elif isinstance(item, list):
+        if check_nesting(item):
+            item = flatten(item)
+    return item
