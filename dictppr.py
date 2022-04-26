16166e8b941f7d7f6341070da81dd3cb3f2a9f25
Dawid Jach <jach.developer@protonmail.com>
2022-04-26 19:46:59 +0200

    Rename inner functions to 'private' convention

--- dictppr.py
+++ dictppr.py
@@ -1,4 +1,4 @@
-def check_nesting(items: list) -> int:
+def _check_nesting(items: list) -> int:
     """
     Checks how many nests are left.
 
@@ -16,11 +16,11 @@ def check_nesting(items: list) -> int:
         if isinstance(item, dict):
             count += 1
         if isinstance(item, list):
-            count += check_nesting(item)
+            count += _check_nesting(item)
     return count
 
 
-def get_new(item: any) -> any:
+def _get_new(item: any) -> any:
     """
     Checks whether the given item
     is as flattened as possible.
@@ -36,20 +36,20 @@ def get_new(item: any) -> any:
     """
     if isinstance(item, dict):
         keys = list(item.keys())
-        item = flatten(item)
-        item = get_new(item)
+        item = _flatten(item)
+        item = _get_new(item)
         if isinstance(item, list) and len(keys) > 1:
-            if not check_nesting(item):
+            if not _check_nesting(item):
                 item = map(lambda k, i: f"{k}:{i}", keys, item)
                 item = map(lambda i: i.replace("'", ""), item)
                 item = list(item)
     elif isinstance(item, list):
-        if check_nesting(item):
-            item = flatten(item)
+        if _check_nesting(item):
+            item = _flatten(item)
     return item
 
 
-def flatten(item: list | dict) -> list | dict:
+def _flatten(item: list | dict) -> list | dict:
     """
     Flattens the given item.
 
@@ -64,21 +64,21 @@ def flatten(item: list | dict) -> list | dict:
     """
     length = len(item)
     if length == 0:
-        flattened = ""
+        _flattened = ""
     elif length == 1:
         if isinstance(item, list):
-            flattened = item[0]
+            _flattened = item[0]
         elif isinstance(item, dict):
-            flattened = list(item.values())[0]
+            _flattened = list(item.values())[0]
     elif length > 1:
         if isinstance(item, list):
-            flattened = [get_new(elem) for elem in item]
+            _flattened = [_get_new(elem) for elem in item]
         elif isinstance(item, dict):
-            flattened = list(item.values())
-    return flattened
+            _flattened = list(item.values())
+    return _flattened
 
 
-def to_string(item: any) -> str:
+def _to_string(item: any) -> str:
     """
     Converts the given item to a string.
 
@@ -111,6 +111,6 @@ def dictppr(dictionary: dict) -> dict:
     ----------
     A flattened dictionary.
     """
-    dictionary = {key: get_new(val) for key, val in dictionary.items()}
-    dictionary = {key: to_string(val) for key, val in dictionary.items()}
+    dictionary = {key: _get_new(val) for key, val in dictionary.items()}
+    dictionary = {key: _to_string(val) for key, val in dictionary.items()}
     return dictionary
