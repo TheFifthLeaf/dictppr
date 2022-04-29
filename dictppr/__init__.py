924779ec92d4bd710a019b37dea5cd7c701a5552
Dawid Jach <jach.developer@protonmail.com>
2022-04-29 19:11:34 +0200

    Add package docstring

--- dictppr/__init__.py
+++ dictppr/__init__.py
@@ -1,3 +1,25 @@
+"""
+A package that allows easy-to-read conversion
+of a dictionary to a string and pretty printing.
+
+VARIABLES
+
+    __version__
+        Contains the package version.
+
+FUNCTONS
+
+    convert(dictionary)
+        Returns a dictionary with flattened elements.
+
+    get(dictionary)
+        Returns a string from the whole dictionary.
+
+    pprint(dictionary)
+        Flattens the dictionary and pretty prints.
+
+"""
+
 from dictppr.dictppr import get, pprint, convert
 
 # Block access to submodule and it other functions
